cb3c7482563621d4081c21defcbcd94696c50615
Leoalves007 <leonardo.fuzinelli@aluno.senai.br>
2025-07-31 10:34:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numero1 = 2;
+        var numero2 = 3;
+
+        var soma = numero1 + numero2;
+
+        Console.WriteLine("Olá, Leonardo!");
+        Console.WriteLine("Bem vindo ao meu Mundo de Programação C#");
+
+        Console.WriteLine("A soma é igual a:" + soma);
+    }
+}
